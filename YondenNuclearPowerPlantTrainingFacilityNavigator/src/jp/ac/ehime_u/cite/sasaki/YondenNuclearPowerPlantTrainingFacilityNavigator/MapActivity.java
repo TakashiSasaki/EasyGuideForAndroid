06d065e2a62d267f3de8855d6fb90da3c752e45e
Takashi <Takashi@rainbow2>
2011-06-12 15:57:28 +0900

    アプリケーションのプロジェクトと、テスト用アプリケーションのプロジェクトをコミット。画面を作成開始。

---
+++ YondenNuclearPowerPlantTrainingFacilityNavigator/src/jp/ac/ehime_u/cite/sasaki/YondenNuclearPowerPlantTrainingFacilityNavigator/MapActivity.java
@@ -0,0 +1,13 @@
+package jp.ac.ehime_u.cite.sasaki.YondenNuclearPowerPlantTrainingFacilityNavigator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

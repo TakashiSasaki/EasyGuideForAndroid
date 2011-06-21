aad4ffe067a9db3de59e6be8d6ee82aec06fb3d3
Takashi <Takashi@rainbow2>
2011-06-21 09:34:00 +0900

    フロアマップとルームマップのサンプル画像を追加。

--- eclipse.bat
+++ eclipse.bat
@@ -1,8 +1,11 @@
+SET ECLIPSE="C:\Program Files\eclipse-platform-3.6.2-win32\eclipse\eclipse.exe"
+IF EXIST %ECLIPSE% (%ECLIPSE% -data . ; goto exit)
 SET ECLIPSE="C:\Program Files\pleiades-e3.6-platform-jre_20100623\eclipse\eclipse.exe"
-IF EXIST %ECLIPSE% (%ECLIPSE% -data . ; exit)
+IF EXIST %ECLIPSE% (%ECLIPSE% -data . ; goto exit)
 SET ECLIPSE="C:\Program Files (x86)\pleiades-e3.6-ultimate-jre_20101005\eclipse\eclipse.exe"
-IF EXIST %ECLIPSE% (%ECLIPSE% -data . ; exit)
+IF EXIST %ECLIPSE% (%ECLIPSE% -data . ; goto exit)
 SET ECLIPSE="C:\Program Files (x86)\pleiades-e3.6-platform-jre_20101025\eclipse\eclipse.exe"
-IF EXIST %ECLIPSE% (%ECLIPSE% -data . ; exit)
+IF EXIST %ECLIPSE% (%ECLIPSE% -data . ; goto exit)
 SET ECLIPSE="C:\Program Files\pleiades-e3.6-platform-jre_20101025\eclipse\eclipse.exe"
-IF EXIST %ECLIPSE% (%ECLIPSE% -data . ; exit)
+IF EXIST %ECLIPSE% (%ECLIPSE% -data . ; goto exit)
+:exit

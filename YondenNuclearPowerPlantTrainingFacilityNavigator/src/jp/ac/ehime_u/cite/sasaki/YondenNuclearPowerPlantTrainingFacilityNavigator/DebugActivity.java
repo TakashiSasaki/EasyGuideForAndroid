5b3b6c6ee1a524ba9e48a36e09d18cdfa695ce5c
Takashi SASAKI <sasaki@cite.ehime-u.ac.jp>
2011-08-10 12:25:20 +0900

    Storage directoryとData directoryを確認。ASUS Transformer では前者は /mnt/sdcard だった。

--- YondenNuclearPowerPlantTrainingFacilityNavigator/src/jp/ac/ehime_u/cite/sasaki/YondenNuclearPowerPlantTrainingFacilityNavigator/DebugActivity.java
+++ YondenNuclearPowerPlantTrainingFacilityNavigator/src/jp/ac/ehime_u/cite/sasaki/YondenNuclearPowerPlantTrainingFacilityNavigator/DebugActivity.java
@@ -5,13 +5,19 @@ import java.io.File;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.widget.TextView;
 
 public class DebugActivity extends Activity {
 	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.debug);
-		//File storage_directory = Environment.getExternalStorageDirectory();
-		//File data_directory = Environment.getDataDirectory();
+		File storage_directory = Environment.getExternalStorageDirectory();
+		TextView textview_debug = (TextView) findViewById(R.id.textViewDebug);
+		textview_debug.append("\n" + "Storage Directory = "
+				+ storage_directory.getPath() + "\n");
+		File data_directory = Environment.getDataDirectory();
+		textview_debug.append("Data Directory = " + data_directory.getParent()
+				+ "\n");
 	}
 }

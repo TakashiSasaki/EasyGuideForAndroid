d88697df625dcf6ef05c33dd9889f7422982744a
Takashi SASAKI <sasaki@cite.ehime-u.ac.jp>
2011-09-30 13:10:54 +0900

    Starting to develop EasyGuideDownloader.

---
+++ EasyGuideDownloader/src/jp/ac/ehime_u/cite/sasaki/easyguide/EasyGuideDownloaderActivity.java
@@ -0,0 +1,13 @@
+package jp.ac.ehime_u.cite.sasaki.easyguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EasyGuideDownloaderActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

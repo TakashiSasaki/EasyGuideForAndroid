b1810cdb5419593747c46cc96ffee6889d6979ee
sasaki <>
2011-10-07 22:16:40 +0900

    refactoring

--- EasyGuideLib/src/jp/ac/ehime_u/cite/sasaki/easyguide/model/UnzippedDirectory.java
+++ EasyGuideLib/src/jp/ac/ehime_u/cite/sasaki/easyguide/model/UnzippedDirectory.java
@@ -1,6 +1,7 @@
 package jp.ac.ehime_u.cite.sasaki.easyguide.model;
 
 import java.io.File;
+import java.util.Date;
 
 /**
  * @author Takashi SASAKI {@link "http://twitter.com/TakashiSasaki"}
@@ -9,6 +10,7 @@ import java.io.File;
 public class UnzippedDirectory {
 
 	private File unzippedDirectory;
+	private Date modfiedDateTime;
 
 	@SuppressWarnings({ "javadoc", "serial" })
 	public static class Exception extends RuntimeException {
@@ -29,14 +31,8 @@ public class UnzippedDirectory {
 			throw new Exception(file.getPath() + " is not readable.");
 		}
 		this.unzippedDirectory = file;
-	}
-
-	/**
-	 * @return the unzippedDirectory
-	 */
-	public File getUnzippedDirectory() {
-		return unzippedDirectory;
-	}
+		this.modfiedDateTime = new Date(file.lastModified());
+	}// an constructor
 
 	/**
 	 * this function was copied a code fragment on
@@ -47,25 +43,40 @@ public class UnzippedDirectory {
 	 * @return true if succeeded
 	 */
 	static public boolean RemoveAllFiles(File path) {
-		if (path.exists()) {
-			if (path.isDirectory()) {
-				File[] files = path.listFiles();
-				for (int i = 0; i < files.length; i++) {
-					if (files[i].isDirectory()) {
-						RemoveAllFiles(files[i]);
-					} else {
-						files[i].delete();
-					}
-				}
-			}
+		if (!path.exists()) {
+			return true;
 		}
+		if (path.isDirectory()) {
+			for (File file : path.listFiles()) {
+				if (file.isDirectory()) {
+					RemoveAllFiles(file);
+				} else {
+					file.delete();
+				}// if
+			}// for
+		}// if
 		return (path.delete());
-	}
+	}// RemoveAllFiles
 
 	/**
 	 * remove all files recursively
 	 */
 	public void RemoveAllFiles() {
 		RemoveAllFiles(this.unzippedDirectory);
+	}// RemoveAllFiles
+
+	/**
+	 * @return the modfiedDateTime
+	 */
+	public Date getModfiedDateTime() {
+		return modfiedDateTime;
 	}
+
+	/**
+	 * @return the unzippedDirectory
+	 */
+	public File getUnzippedDirectory() {
+		return unzippedDirectory;
+	}
+
 }

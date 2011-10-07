f5aeb25dc85ba3cd833e232ff9d5ad9b9f2ace2c
sasaki <>
2011-10-07 20:36:04 +0900

    add checking for malformed directory name in a constructor.

--- EasyGuideLib/src/jp/ac/ehime_u/cite/sasaki/easyguide/model/DirectoryName.java
+++ EasyGuideLib/src/jp/ac/ehime_u/cite/sasaki/easyguide/model/DirectoryName.java
@@ -21,10 +21,18 @@ public class DirectoryName {
 	 */
 	public DirectoryName(String directory_name) {
 		String[] parts = directory_name.split("[ ]+");
-		this.number = Integer.parseInt(parts[0]);
-		this.name = parts[1];
-		this.x = Integer.parseInt(parts[2]);
-		this.y = Integer.parseInt(parts[3]);
+		try {
+			this.number = Integer.parseInt(parts[0]);
+			this.name = parts[1];
+			this.x = Integer.parseInt(parts[2]);
+			this.y = Integer.parseInt(parts[3]);
+		} catch (NumberFormatException e) {
+			throw new Exception("Invalid directory name " + directory_name
+					+ ", " + e.getMessage());
+		} catch (IndexOutOfBoundsException e) {
+			throw new Exception("Invalid directory name " + directory_name
+					+ ", " + e.getMessage());
+		}
 	}
 
 	/**

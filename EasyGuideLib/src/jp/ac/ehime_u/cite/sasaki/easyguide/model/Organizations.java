5ee86fe2bc84a97c6cb3c1c30685aec4804bc5a9
sasaki <>
2011-10-07 21:55:22 +0900

    class Organization and class Domain see the same level.

--- EasyGuideLib/src/jp/ac/ehime_u/cite/sasaki/easyguide/model/Organizations.java
+++ EasyGuideLib/src/jp/ac/ehime_u/cite/sasaki/easyguide/model/Organizations.java
@@ -7,70 +7,31 @@ import java.util.Comparator;
 import android.util.Log;
 
 /**
- * 複数の施設を表すシングルトンクラス。 インスタンスを生成すると自動的に外部記憶を走査して施設の一覧を作成する。
- * 
  * @author Takashi SASAKI {@link "http://twitter.com/TakashiSasaki"}
  */
 @SuppressWarnings("serial")
 public class Organizations extends ArrayList<Organization> {
-
-	// public ArrayList<Organization> organizations = new
-	// ArrayList<Organization>();
 
 	private static Organizations theOrganizations;
 
 	private Organizations() {
 		super();
 		ScanRootDirectory();
-
-		class OrganizationComparator implements Comparator<Organization> {
-			public int compare(Organization object1, Organization object2) {
-				if (object1.getOrganizationOrder() < object2
-						.getOrganizationOrder()) {
-					return -1;
-				} else if (object1.getOrganizationOrder() > object2
-						.getOrganizationOrder()) {
-					return 1;
-				} else {
-					return 0;
-				}
-			}
-		}
-
-		Collections.sort(this, new OrganizationComparator());
-	}
+	}// a constructor
 
 	private void ScanRootDirectory() {
-		Log.v(this.getClass().getSimpleName(),"Enumerating domain directories");
-		File[] domain_directories = Root.GetTheRoot().getRootDirectory()
-				.listFiles();
-		for (int i = 0; i < domain_directories.length; ++i) {
-			File domain_directory = domain_directories[i];
-			Log.v(this.getClass().getSimpleName(), domain_directory.getPath());
-			if (!domain_directory.isDirectory())
+		Log.v(this.getClass().getSimpleName(),
+				"Enumerating organization directories");
+		for (File organization_directory : Root.GetTheRoot().getRootDirectory()
+				.listFiles()) {
+			Log.v(this.getClass().getSimpleName(),
+					organization_directory.getPath());
+			if (!organization_directory.isDirectory()) {
 				continue;
-			if (!domain_directory.canRead())
-				continue;
-			Domain domain = new Domain(domain_directory);
-			ScanDomainDirectory(domain);
-		}
-	}
-
-	private void ScanDomainDirectory(Domain domain_) {
-		
-		File[] organization_directories = domain_.getDomainDirectory()
-				.listFiles();
-		for (int i = 0; i < organization_directories.length; ++i) {
-			File organization_directory = organization_directories[i];
-			Log.v(this.getClass().getName(), organization_directory.getPath()
-					+ " was found.");
-			if (!organization_directory.isDirectory())
-				continue;
-			if (!organization_directory.canRead())
-				continue;
+			}
 			this.add(new Organization(organization_directory));
-		}
-	}
+		}// for
+	}// ScanRootDirectory
 
 	/**
 	 * @return singleton object of Organizations
@@ -80,5 +41,5 @@ public class Organizations extends ArrayList<Organization> {
 			theOrganizations = new Organizations();
 		}
 		return theOrganizations;
-	}
-}
+	}// GetTheOrganizations
+}// Organizations

933a3fa052ffeba87e3c99980d85e79b20624b85
sasaki <>
2011-10-08 15:38:04 +0900

    refactoring

--- EasyGuideLib/src/jp/ac/ehime_u/cite/sasaki/easyguide/model/Facility.java
+++ EasyGuideLib/src/jp/ac/ehime_u/cite/sasaki/easyguide/model/Facility.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.util.Log;
 
 /**
@@ -16,10 +15,8 @@ public class Facility extends ArrayList<Building> {
 
 	private static final String facilityImageName = "facility.png";
 	private File facilityDirectory;
-	private int facilityNumber;
-	private String facilityName;
-	private Bitmap facilityBitmap;
-	private int facilityX, facilityY;
+	private DirectoryName facilityDirectoryName;
+	private DirectoryImage facilityDirectoryImage;
 
 	/**
 	 * @param facility_directory
@@ -28,74 +25,45 @@ public class Facility extends ArrayList<Building> {
 		super();
 		this.facilityDirectory = facility_directory;
 		// String building_directory_path = building_directory.getPath();
-		DirectoryName directory_name = new DirectoryName(
+		this.facilityDirectoryName = new DirectoryName(
 				facility_directory.getName());
-		this.facilityNumber = directory_name.getNumber();
-		this.facilityName = directory_name.getName();
-		this.facilityX = directory_name.getX();
-		this.facilityY = directory_name.getY();
-		File facility_image_file = new File(facility_directory,
-				Facility.facilityImageName);
-		this.facilityBitmap = BitmapFactory.decodeFile(facility_image_file
-				.getPath());
-		ScanBuildings();
-	}
-
-	private void ScanBuildings() {
+		this.facilityDirectoryImage = new DirectoryImage(facilityDirectory,
+				facilityImageName);
 		Log.v(this.getClass().getSimpleName(),
 				"Scanning building directories in "
 						+ this.facilityDirectory.getAbsolutePath());
 		for (File building_directory : this.facilityDirectory.listFiles()) {
-			if (!building_directory.isDirectory()) {
-				continue;
-			}
-			Log.v(this.getClass().getSimpleName(), "Building directory "
-					+ building_directory.getAbsolutePath() + " was found.");
-			try {
-				Building building = new Building(building_directory);
-				assert (building != null);
-				this.add(building);
-			} catch (Exception e) {
-				Log.v(this.getClass().getSimpleName(),
-						"an exception was catched while constructing a Building object for "
-								+ building_directory.getAbsolutePath());
-			}// try
+			this.add(new Building(building_directory));
 		}// for
-	}// ScanBuildings
+	}// a constructor
 
-	/**
-	 * @return the facilityNumber
-	 */
+	@SuppressWarnings("javadoc")
 	public int getFacilityNumber() {
-		return facilityNumber;
+		return this.facilityDirectoryName.getNumber();
 	}
 
-	/**
-	 * @return the facilityName
-	 */
+	@SuppressWarnings("javadoc")
 	public String getFacilityName() {
-		return facilityName;
+		return this.facilityDirectoryName.getName();
 	}
 
-	/**
-	 * @return the facilityBitmap
-	 */
-	public Bitmap getFacilityBitmap() {
-		return facilityBitmap;
-	}
-
-	/**
-	 * @return the facilityX
-	 */
+	@SuppressWarnings("javadoc")
 	public int getFacilityX() {
-		return facilityX;
+		return this.facilityDirectoryName.getX();
 	}
 
-	/**
-	 * @return the facilityY
-	 */
+	@SuppressWarnings("javadoc")
 	public int getFacilityY() {
-		return facilityY;
+		return this.facilityDirectoryName.getY();
 	}
 
-}
+	@SuppressWarnings("javadoc")
+	public Bitmap getFacilityImage() {
+		return this.facilityDirectoryImage.getImage();
+	}
+
+	@SuppressWarnings("javadoc")
+	public Bitmap getFacilityThumbnail() {
+		return this.facilityDirectoryImage.getThumbnail();
+	}
+}// Facility

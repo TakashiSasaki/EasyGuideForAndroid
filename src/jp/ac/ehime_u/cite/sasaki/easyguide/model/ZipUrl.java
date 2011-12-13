4c749cff804d4d9887708f50ddc01271015107bf
sasaki <>
2011-12-13 23:54:51 +0900

    ZipUrl handles not URL but URI. It should be renamed later.

--- src/jp/ac/ehime_u/cite/sasaki/easyguide/model/ZipUrl.java
+++ src/jp/ac/ehime_u/cite/sasaki/easyguide/model/ZipUrl.java
@@ -3,6 +3,7 @@ package jp.ac.ehime_u.cite.sasaki.easyguide.model;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Calendar;
@@ -10,42 +11,49 @@ import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import android.content.ContentValues;
+
 public class ZipUrl {
 	private Domain domain;
-	private URL url;
+	private URI uri;
 	private Date downloadedDate;
 	private File downloadedFile;
 	private Date lastModified;
 	private LastModifiedHeaderThread lastModifiedThread;
+	final static public String COLUMN_domain = "domain";
+	final static public String COLUMN_url = "url";
+	final static public String COLUMN_downloadedFile = "downloadedFile";
+	final static public String COLUMN_lastModified = "lastModified";
 
-	public ZipUrl(Domain domain_, URL url_) throws URISyntaxException,
-			MalformedURLException {
-		this.url = url_;
+	public ZipUrl(Domain domain_, URI uri_, File downloaded_file,
+			Date last_modified) throws FileNotFoundException {
 		this.domain = domain_;
-		CheckLastModified();
+		this.uri = uri_;
+		this.downloadedFile = downloaded_file;
+		this.SetDownloadedFile(this.downloadedFile);
+		this.lastModified = last_modified;
+	}// a constructor
+
+	public ZipUrl(Domain domain_, URI uri_) throws URISyntaxException,
+			MalformedURLException {
+		this.uri = uri_;
+		this.domain = domain_;
+		if (!this.uri.getHost().equals("assets")) {
+			CheckLastModified();
+		}// if
 	} // a constructor
 
 	public void CheckLastModified() {
-		if (url.getHost() != "assets") {
+		if (this.uri.getHost() != "assets") {
 			try {
-				this.lastModifiedThread = new LastModifiedHeaderThread(url);
+				this.lastModifiedThread = new LastModifiedHeaderThread(this.uri);
 			} catch (URISyntaxException e) {
 				e.printStackTrace();
 				return;
 			}
-			lastModifiedThread.start();
+			this.lastModifiedThread.start();
 		}// if
 	}// SetLastModified
-
-	public Date getLastModified() throws InterruptedException {
-		if (lastModified != null) {
-			return lastModified;
-		} else {
-			lastModifiedThread.join();
-			this.lastModified = this.lastModifiedThread.getLastModified();
-			return this.lastModified;
-		}// getLastModified
-	}// getLastModified
 
 	public void SetDownloadedFile(File downloaded_file)
 			throws FileNotFoundException {
@@ -69,16 +77,52 @@ public class ZipUrl {
 				+ this.downloadedDate.getTime() + ".zip");
 	}// SetDownloadedFile
 
+	public Domain GetDomain() {
+		return this.domain;
+	}
+
+	public String GetDomainByString() {
+		return this.domain.getDomainDirectory().getName();
+	}// GetDomainByString
+
+	public File GetDomainDirectory() {
+		return this.domain.getDomainDirectory();
+	}
+
+	public URI getUri() {
+		return this.uri;
+	}
+
 	public Date getDownloadedDate() {
 		return this.downloadedDate;
 	}
 
-	public URL getUrl() {
-		return this.url;
-	}
-	
-	public File getDownloadedFile(){
+	public File getDownloadedFile() {
 		return this.downloadedFile;
 	}
-	
+
+	public Date getLastModified() throws InterruptedException {
+		if (this.lastModified != null) {
+			return this.lastModified;
+		} else {
+			this.lastModifiedThread.join();
+			this.lastModified = this.lastModifiedThread.getLastModified();
+			this.lastModifiedThread = null; // release LastModifiedThread object
+			return this.lastModified;
+		}// getLastModified
+	}// getLastModified
+
+	public ContentValues GetContentValues() {
+		ContentValues cv = new ContentValues();
+		cv.put(COLUMN_domain, this.domain.getDomainDirectory().getName());
+		cv.put(COLUMN_downloadedFile, this.downloadedFile.getName());
+		try {
+			cv.put(COLUMN_lastModified, this.lastModified.getTime());
+		} catch (NullPointerException nurupo) {
+			cv.put(COLUMN_lastModified, 0L);
+		}// try
+		cv.put(COLUMN_url, this.uri.toString());
+		return cv;
+	}// GetContentValues
+
 }// ZipUrl

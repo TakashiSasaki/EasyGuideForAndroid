8a5b482a1770d9adb0fd26fbd46131afd164401c
unknown <Takashi@.(none)>
2012-02-08 16:34:51 +0900

    working on TocActivity and related classes.

--- src/jp/ac/ehime_u/cite/sasaki/easyguide/ui/TocActivity.java
+++ src/jp/ac/ehime_u/cite/sasaki/easyguide/ui/TocActivity.java
@@ -1,14 +1,9 @@
 package jp.ac.ehime_u.cite.sasaki.easyguide.ui;
 
 import jp.ac.ehime_u.cite.sasaki.easyguide.R;
-import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.GestureDetector.OnDoubleTapListener;
-import android.view.MotionEvent;
 import android.widget.ListView;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 
 public class TocActivity extends ListActivity {
 	private ListView tocListView;
--- src/jp/ac/ehime_u/cite/sasaki/easyguide/ui/TocAdapter.java
+++ src/jp/ac/ehime_u/cite/sasaki/easyguide/ui/TocAdapter.java
@@ -1,15 +1,21 @@
 package jp.ac.ehime_u.cite.sasaki.easyguide.ui;
 
+import jp.ac.ehime_u.cite.sasaki.easyguide.model.Facility;
+import jp.ac.ehime_u.cite.sasaki.easyguide.model.Organization;
 import jp.ac.ehime_u.cite.sasaki.easyguide.model.Organizations;
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
 public class TocAdapter extends ArrayAdapter<TocArrayItem> {
 
-	Organizations
+	Organizations organizations = Organizations.GetTheOrganizations();
+
 	public TocAdapter(Context context, int resource, int textViewResourceId) {
 		super(context, resource);
-		
-		// TODO Auto-generated constructor stub
-	}
+		for (Organization organization : this.organizations){
+			organization.e
+			for(Facility facility : organization){
+			}
+		}
+	}// TocAdapter
 }

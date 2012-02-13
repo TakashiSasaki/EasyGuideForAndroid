da52fc1acd24deabecb5eac6c6b2a976c118867b
unknown <Takashi@.(none)>
2012-02-13 09:35:50 +0900

    The version number of the database was changed from 1 to 5. Rather the common database should be managed by only one SQLiteOpenHelper.

--- src/jp/ac/ehime_u/cite/sasaki/easyguide/model/DownloadedItemsSQLiteOpenHelper.java
+++ src/jp/ac/ehime_u/cite/sasaki/easyguide/model/DownloadedItemsSQLiteOpenHelper.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DownloadedItemsSQLiteOpenHelper extends SQLiteOpenHelper {
 	Context context;
@@ -15,26 +16,28 @@ public class DownloadedItemsSQLiteOpenHelper extends SQLiteOpenHelper {
 			+ DownloadedItem.COLUMN_DOWNLOADED_FILE + " TEXT NOT NULL;";
 	final static String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
 
-	public DownloadedItemsSQLiteOpenHelper(Context context, String name,
-			CursorFactory factory, int version) {
+	public DownloadedItemsSQLiteOpenHelper(Context context) {
 		// super(context, name, factory, version);
-		super(context, "EasyGuideDownloader.sqlite", null, 1);
+		super(context, "EasyGuideDownloader.sqlite", null, 5);
 		this.context = context;
-		// TODO Auto-generated constructor stub
-	}
+	} // the constructor of DownloadedItemsSQLiteOpenHelper
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
+		Log.v(this.getClass().getSimpleName(), "onCreate");
 		db.execSQL(CREATE_TABLE);
-	}
+	}// onCreatae
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.v(this.getClass().getSimpleName(), "onUpgrade");
 		db.execSQL(DROP_TABLE);
 		this.onCreate(db);
-	}
+	}// onUpgrade
 
 	public void Insert(DownloadedItem downloaded_item) {
+		Log.v(this.getClass().getSimpleName(), "Insert "
+				+ downloaded_item.getDownloadedFile().getPath());
 		Delete(downloaded_item);
 		SQLiteDatabase wdb = getWritableDatabase();
 		wdb.insert(TABLE_NAME, null, downloaded_item.getContentValues());
@@ -42,6 +45,8 @@ public class DownloadedItemsSQLiteOpenHelper extends SQLiteOpenHelper {
 	}
 
 	public void Delete(DownloadedItem downloaded_item) {
+		Log.v(this.getClass().getSimpleName(), "Delete "
+				+ downloaded_item.getDownloadedFile().getPath());
 		SQLiteDatabase wdb = getWritableDatabase();
 		wdb.delete(TABLE_NAME, DownloadedItem.COLUMN_DOWNLOADED_FILE,
 				new String[] { downloaded_item.getDownloadedFile().getPath() });
@@ -49,7 +54,9 @@ public class DownloadedItemsSQLiteOpenHelper extends SQLiteOpenHelper {
 	}
 
 	public Cursor Select() {
+		Log.v(this.getClass().getSimpleName(), "Select: getting readable database.");
 		SQLiteDatabase rdb = getReadableDatabase();
+		Log.v(this.getClass().getSimpleName(), "Select: querying");
 		return rdb.query(TABLE_NAME, new String[] {
 				DownloadedItem.COLUMN_DOWNLOADED_DATE,
 				DownloadedItem.COLUMN_DOWNLOADED_FILE }, null, null, null,

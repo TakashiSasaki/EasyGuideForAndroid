3cfb9c8151819132a0abd79ea6178916a7445d4a
unknown <Takashi@.(none)>
2012-02-15 18:35:05 +0900

    refactoring on easyguide.util.Log

--- src/jp/ac/ehime_u/cite/sasaki/easyguide/util/Log.java
+++ src/jp/ac/ehime_u/cite/sasaki/easyguide/util/Log.java
@@ -2,11 +2,12 @@ package jp.ac.ehime_u.cite.sasaki.easyguide.util;
 
 public class Log {
 	static public void v(Throwable t, String message) {
-		String file_name = t.getStackTrace()[0].getFileName();
-		int line_numer = t.getStackTrace()[0].getLineNumber();
-		String class_name = t.getStackTrace()[0].getClassName();
-		String method = t.getStackTrace()[0].getMethodName();
-		String simple_name = t.getStackTrace()[0].getClass().getSimpleName();
+		StackTraceElement top_of_stack = t.getStackTrace()[0];
+		String file_name = top_of_stack.getFileName();
+		int line_numer = top_of_stack.getLineNumber();
+		String class_name = top_of_stack.getClassName();
+		String method = top_of_stack.getMethodName();
+		String simple_name = top_of_stack.getClass().getSimpleName();
 		String atline = "at " + class_name + "." + method + "(" + file_name
 				+ ":" + line_numer + ")";
 		android.util.Log.v(simple_name, atline);

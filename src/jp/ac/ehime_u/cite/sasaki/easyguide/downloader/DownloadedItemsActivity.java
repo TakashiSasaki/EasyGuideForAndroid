365ed03794612a86df0ff27205d41ccb013e2a03
unknown <Takashi@.(none)>
2012-02-15 18:31:56 +0900

    Insert an instance of DownloadedItem in DownloadedItemsActivity.

--- src/jp/ac/ehime_u/cite/sasaki/easyguide/downloader/DownloadedItemsActivity.java
+++ src/jp/ac/ehime_u/cite/sasaki/easyguide/downloader/DownloadedItemsActivity.java
@@ -1,7 +1,11 @@
 package jp.ac.ehime_u.cite.sasaki.easyguide.downloader;
 
+import java.util.ArrayList;
+
 import jp.ac.ehime_u.cite.sasaki.easyguide.db.DownloadedItemTable;
 import jp.ac.ehime_u.cite.sasaki.easyguide.db.SimpleCursorLoader;
+import jp.ac.ehime_u.cite.sasaki.easyguide.download.Domain;
+import jp.ac.ehime_u.cite.sasaki.easyguide.download.DownloadedItem;
 import jp.ac.ehime_u.cite.sasaki.easyguide.model.Root;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Context;
@@ -32,6 +36,10 @@ public class DownloadedItemsActivity extends CommonMenuActivity implements
 				.getSystemService(LAYOUT_INFLATER_SERVICE);
 		this.downloadedItemsTable = new DownloadedItemTable(this);
 		Root.GetTheRoot().EnumerateDomainDirectories();
+		for(Domain d : Root.GetTheRoot()){
+			ArrayList<DownloadedItem> adi = d.ScanDownloadedItems();
+			DownloadedItemTable.getInstance(this).Insert(adi);
+		}
 		SetListViewDownloadedItems();
 	}// onCreate
 

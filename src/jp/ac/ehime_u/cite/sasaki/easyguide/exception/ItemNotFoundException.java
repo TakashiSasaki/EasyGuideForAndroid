3a590270f38959a79bb4e70c1c37ce5df1bc29b5
unknown <Takashi@.(none)>
2012-02-18 11:59:30 +0900

    ItemNotFoundException was introduced.

---
+++ src/jp/ac/ehime_u/cite/sasaki/easyguide/exception/ItemNotFoundException.java
@@ -0,0 +1,7 @@
+package jp.ac.ehime_u.cite.sasaki.easyguide.exception;
+
+public class ItemNotFoundException extends Exception {
+	public ItemNotFoundException(String message) {
+		super(message);
+	}
+}

5a4f1581c6fb1fa13c878cfcecd4b0f1728ec225
Takashi SASAKI <takashi316@gmail.com>
2012-06-17 21:07:52 +0900

    Detector imported

---
+++ Detector/gen/jp/ac/ehime_u/cite/sasaki/easyguide/detector/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jp.ac.ehime_u.cite.sasaki.easyguide.detector;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}

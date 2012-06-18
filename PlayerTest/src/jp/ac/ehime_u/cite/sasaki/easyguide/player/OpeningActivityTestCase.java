a3bce1c60e5a32fac052063e38f021dd2b5f15c3
Takashi SASAKI <takashi316@gmail.com>
2012-06-18 13:15:58 +0900

    a skelton of test case

---
+++ PlayerTest/src/jp/ac/ehime_u/cite/sasaki/easyguide/player/OpeningActivityTestCase.java
@@ -0,0 +1,50 @@
+package jp.ac.ehime_u.cite.sasaki.easyguide.player;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+
+import jp.ac.ehime_u.cite.sasaki.easyguide.player.OpeningActivity;
+
+public class OpeningActivityTestCase extends
+		ActivityInstrumentationTestCase2<OpeningActivity> {
+
+	public OpeningActivityTestCase(String pkg,
+			Class<OpeningActivity> activityClass) {
+		super(pkg, activityClass);
+		// TODO Auto-generated constructor stub
+	}
+
+	public OpeningActivityTestCase() {
+		super("jp.ac.ehime_u.cite.sasaki.easyguide.player.OpeningActivit",
+				OpeningActivity.class);
+	}
+
+	public void test1() {
+		this.activity.runOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+				OpeningActivityTestCase.this.buttonResetUsers.requestFocus();
+				getInstrumentation().waitForIdle(null);
+			}
+		});
+
+	}
+
+	Activity activity;
+	Button buttonResetUsers;
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		this.activity = getActivity();
+		super.setUp();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}
+}
--- PlayerTest/src/jp/ac/ehime_u/cite/sasaki/easyguide/player/test/OpeningActivityTestCase.java
+++
@@ -1,7 +0,0 @@
-package jp.ac.ehime_u.cite.sasaki.easyguide.player.test;
-
-import junit.framework.TestCase;
-
-public class OpeningActivityTestCase extends TestCase {
-
-}

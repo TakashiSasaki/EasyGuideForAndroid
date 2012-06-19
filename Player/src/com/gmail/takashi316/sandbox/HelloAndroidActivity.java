6cf0779a2d7887cad05358441681cf12a59b9542
Takashi SASAKI <takashi316@gmail.com>
2012-06-19 20:43:20 +0900

    HelloAndroidActivity extending SmartActivity passed the test

--- Player/src/com/gmail/takashi316/sandbox/HelloAndroidActivity.java
+++ Player/src/com/gmail/takashi316/sandbox/HelloAndroidActivity.java
@@ -4,10 +4,9 @@ import com.gmail.takashi316.lib.android.activity.SmartActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import jp.ac.ehime_u.cite.sasaki.easyguide.R;
 
-public class HelloAndroidActivity extends FragmentActivity {
+public class HelloAndroidActivity extends SmartActivity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {

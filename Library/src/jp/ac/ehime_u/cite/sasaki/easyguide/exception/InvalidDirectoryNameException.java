ff4a68a0bc2a5cdd9c3a278b4fa1eb7bf2b8d6b0
Takashi SASAKI <takashi316@gmail.com>
2012-09-26 04:25:16 +0900

    Recursive behavior of ContentUnit is well.

--- Library/src/jp/ac/ehime_u/cite/sasaki/easyguide/exception/InvalidDirectoryNameException.java
+++ Library/src/jp/ac/ehime_u/cite/sasaki/easyguide/exception/InvalidDirectoryNameException.java
@@ -1,7 +1,7 @@
 package jp.ac.ehime_u.cite.sasaki.easyguide.exception;
 
-public class InvalidDirectoryNameException extends RuntimeException{
-	public InvalidDirectoryNameException(String directory_name){
-		super(directory_name);
+public class InvalidDirectoryNameException extends RuntimeException {
+	public InvalidDirectoryNameException(String message) {
+		super(message);
 	}
 }

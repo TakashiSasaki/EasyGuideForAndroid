b3682fbd114d9ee4406410e8e152acf739a1e28c
Takashi SASAKI <takashi316@gmail.com>
2013-01-09 15:44:41 +0900

    Google Maps Android API v2 sample code

---
+++ Player/src/com/gmail/takashi316/sandbox/MapsV2Activity.java
@@ -0,0 +1,15 @@
+package com.gmail.takashi316.sandbox;
+
+import com.gmail.takashi316.easyguide.player.R;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MapsV2Activity extends FragmentActivity {
+	@Override
+	protected void onCreate(Bundle saved_instance_state) {
+		super.onCreate(saved_instance_state);
+		setContentView(R.layout.maps_v2_activity);
+	}
+}// MapsV2Activity
+
